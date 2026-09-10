8d071e93ae4795ed7877f2b9fe07aa9d1c5c32c2
FTR16 <assyfaturrohman.akbar@gmail.com>
2026-09-10 09:29:22 +0700

    Tugas Array
    Assyfaturrohman Akbar
    XI PPLG 2

---
+++ Array.php
@@ -0,0 +1,20 @@
+<?php
+
+$dataanakherman = [
+
+    "Nama" => "GYZY",
+    "nis" => "00000",
+    "usia" => "1",
+    "hobi" => "Ngaloco",
+
+];
+
+echo "Nama" , $dataanakherman["Nama"];
+echo "<br/>";
+echo "nis" , $dataanakherman["nis"];
+echo "<br/>";
+echo "usia" , $dataanakherman["usia"];
+echo "<br/>";
+echo "hobi" , $dataanakherman["hobi"];
+
+?>
